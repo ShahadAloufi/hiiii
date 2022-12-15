d986052bf5bf3ab55ef34bec79f9e0cc2234a053
ShahadAloufi <shahdse001@gmail.com>
2022-12-15 08:54:56 +0300

    Initial Commit

---
+++ hiiii/hiiiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiiiiApp.swift
+//  hiiii
+//
+//  Created by shahadmufleh on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct hiiiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
